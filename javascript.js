6a6048771f790161bea8d9e67f5ba340fbf30c35
Henry Moore <hmoore@basecampcodingacademy.org>
2018-11-06 16:11:57 -0600

    Built more templates to display more pages

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+var PAGE_DATA = {};
+
 let main = document.querySelector("main");
 
 function renderPage(main, id) {
@@ -5,7 +7,7 @@ function renderPage(main, id) {
     main.innerHTML = page;
 }
 
-function renderHomePage(main) {
+function renderHomePage(main, PAGE_DATA) {
     renderPage(main, "homePage");
 
     let signUpBtn = document.querySelector("#signUpBtn");
@@ -15,7 +17,7 @@ function renderHomePage(main) {
         renderPage(main, "registerPage");
         document.querySelector("button").addEventListener("click", event => {
             event.preventDefault();
-            signUp();
+            signUp(PAGE_DATA);
         });
     });
 
@@ -23,12 +25,12 @@ function renderHomePage(main) {
         renderPage(main, "loginPage");
         document.querySelector("button").addEventListener("click", event => {
             event.preventDefault();
-            login();
+            login(PAGE_DATA);
         });
     });
 }
 
-function signUp() {
+function signUp(data) {
     let username = document.querySelector("#username").value;
     let password = document.querySelector("#password").value;
     let passwordRepeat = document.querySelector("#passwordRepeat").value;
@@ -45,7 +47,9 @@ function signUp() {
     })
         .then(response => response.json())
         .then(obj => {
-            console.log(obj);
+            data.token = obj.token;
+            data.username = username;
+            renderUserHome(main, PAGE_DATA);
         })
         .catch(e => {
             console.log(e);
@@ -53,7 +57,7 @@ function signUp() {
         });
 }
 
-function login() {
+function login(data) {
     let username = document.querySelector("#username").value;
     let password = document.querySelector("#password").value;
     fetch(`https://bcca-pingpong.herokuapp.com/api/login/`, {
@@ -66,8 +70,12 @@ function login() {
             password: password
         })
     })
-        .then(response => {
-            console.log(response);
+        .then(response => response.json())
+        .then(obj => {
+            data.token = obj.token;
+            console.log(data.token);
+            data.username = username;
+            renderUserHome(main, PAGE_DATA);
         })
         .catch(e => {
             console.log(e);
@@ -75,4 +83,88 @@ function login() {
         });
 }
 
-renderHomePage(main);
+function getUsers(token, PAGE_DATA) {
+    return fetch("https://bcca-pingpong.herokuapp.com/api/users/", {
+        method: "GET",
+        headers: { Authorization: `Token ${token}` }
+    })
+        .then(response => response.json())
+        .then(obj => {
+            return (PAGE_DATA.users = obj);
+        })
+        .catch(e => {
+            console.log(e);
+            console.log(e.message);
+        });
+}
+
+function renderUserHome(main, PAGE_DATA) {
+    var source = document.getElementById("userHomePage").innerHTML;
+    var template = Handlebars.compile(source);
+
+    getUsers(PAGE_DATA.token, PAGE_DATA).then(() => {
+        var usernames = [];
+        for (var user of PAGE_DATA.users) {
+            usernames.push(user.username);
+        }
+        console.log(usernames);
+        var html = template({
+            user: PAGE_DATA.username,
+            players: PAGE_DATA.users
+        });
+        main.innerHTML = "";
+        main.insertAdjacentHTML("beforeend", html);
+
+        document.getElementById("newGameBtn").addEventListener("click", () => {
+            renderNewGame(PAGE_DATA, usernames);
+        });
+        return usernames;
+    });
+}
+
+function inputPlayers(PAGE_DATA) {
+    PAGE_DATA.users = getUsers(PAGE_DATA.token);
+    console.log(PAGE_DATA.users);
+}
+
+function renderNewGame(PAGE_DATA, usernames) {
+    var source = document.getElementById("gameWindow").innerHTML;
+    var template = Handlebars.compile(source);
+    var html = template({
+        player1: PAGE_DATA.player_1,
+        player2: PAGE_DATA.player_2
+    });
+    var block = document.querySelector("#gameArea");
+    block.innerHTML = "";
+    block.insertAdjacentHTML("beforeend", html);
+    isVal(usernames);
+}
+function isVal(usernames) {
+    var player1 = document.getElementById("p1USER");
+    var player2 = document.getElementById("p2USER");
+    player1.addEventListener("input", () => {
+        checkUser(usernames);
+        console.log(usernames);
+    });
+    player2.addEventListener("input", () => {
+        checkUser(usernames);
+    });
+}
+
+function checkUser(usernames) {
+    var player1 = document.getElementById("p1USER").value;
+    var player2 = document.getElementById("p2USER").value;
+    console.log(player1);
+    var btn = document.getElementById("play");
+    if (usernames.includes(player1) && usernames.includes(player2)) {
+        btn.removeAttribute("disabled");
+    }
+    // playBtn(usernames)
+}
+// function playBtn(usernames){
+//     var btn = document.getElementById("play");
+//     btn.addEventListener("click", ()=> {
+
+//     })
+// }
+renderHomePage(main, PAGE_DATA);
