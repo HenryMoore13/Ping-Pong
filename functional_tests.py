0cc853dcba1527b9d782947cb7e31e0ba2973e5d
Henry Moore <hmoore@basecampcodingacademy.org>
2018-11-08 15:19:55 -0600

    added more test

--- functional_tests.py
+++ functional_tests.py
@@ -25,3 +25,25 @@ class TestLogin:
             EC.presence_of_element_located((By.ID, "newGameBtn")))
         assert "Start Game" in self.browser.find_element_by_id(
             "newGameBtn").text
+
+    def test_game(self):
+        self.browser.get("http://localhost:5500/")
+        self.browser.find_element_by_id("loginBtn").click()
+        self.browser.find_element_by_id("username").send_keys("henrymoore")
+        self.browser.find_element_by_id("password").send_keys("henry")
+        self.browser.find_element_by_id("logBtn").click()
+        WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located((By.ID, "newGameBtn")))
+        assert "Start Game" in self.browser.find_element_by_id(
+            "newGameBtn").text
+        self.browser.find_element_by_id("newGameBtn").click()
+        self.browser.find_element_by_id("p1USER").send_keys("henrymoore")
+        self.browser.find_element_by_id("p2USER").send_keys("henrymoore13")
+        self.browser.find_element_by_id("play").click()
+        for _ in range(9):
+            self.browser.find_element_by_id("ply1UCount").click()
+        for _ in range(10):
+            self.browser.find_element_by_id("ply2UCount").click()
+        WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located((By.ID, "startNew")))
+        assert "New Game" in self.browser.find_element_by_id("startNew").text
